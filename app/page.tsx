c9d8175d03a5c93d5e8460a54b830a1bdbb3ef20
praduman04 <kumarpraduman97@gmail.com>
2023-08-05 15:21:47 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <section className='flex-start flex-col padding mb-16'>
+        
+        <h1>categories</h1>
+        <h1>posts</h1>
+        <h1>loadMore</h1>
+
+    </section>
+  )
+}
+
+export default Home;
---
+++ components/AuthProviders.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const AuthProviders = () => {
+  return (
+   <div>
+    AuthProviders
+   </div>
+
+  )
+}
+
+export default AuthProviders
